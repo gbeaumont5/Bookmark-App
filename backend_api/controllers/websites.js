fffeb963616b38b159e8f03d80ea7a6ecee7c88f
Gary Beaumont <garyallenbeaumont@gmail.com>
2019-08-15 18:12:39 -0400

    created 'create' delete and update routes

--- backend_api/controllers/websites.js
+++ backend_api/controllers/websites.js
@@ -2,7 +2,7 @@ const express = require('express');
 const websites = express.Router();
 const Websites = require('../models/websites')
 
-//INDEXXXX
+//INDEX
 websites.get('/', (req, res) => {
     Websites.find({}, (err, foundWebsites) => {
         if (err) {
@@ -15,12 +15,36 @@ websites.get('/', (req, res) => {
 
 
 //Create
+websites.post('/', (req, res) => {
+    Websites.create(req.body, (error, createdWebsite) => {
+      if (error) {
+        res.status(400).json({ error: error.message });
+      }
+      res.status(200).send(createdWebsite);
+    });
+  });
 
 //Delete
-
+// websites.delete('/:id', (req, res) => {
+//     Websites.findByIdAndRemove(req.params.id, (err, deletedWebsites) => {
+//         if (err) {
+//             res.status(400).json({error: err.message})
+//         }
+//         res.status(200).json(deletedWebsites)
+//     })
+// })
 
 
 //Update
+// websites.put('/:id', (req, res) => {
+//     Websites.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedWebsites) => {
+//         if (err) {
+//             res.status(400).json({error: err.message})
+//         }
+//         res.status(200).json(updatedWebsites)
+//     })
+// })
+
 
 
 
