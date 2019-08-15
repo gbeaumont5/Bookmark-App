2aec84d1983d66406227c93a13630210ed459e14
haroldjohan <haroldmiranda1@Harolds-MacBook-Air.local>
2019-08-15 17:20:36 -0400

    added a create route to controllers

--- backend_api/controllers/websites.js
+++ backend_api/controllers/websites.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const websites = express.Router();
-const Websites = require('../models/websites')
+const Websites = require('../models/websites');
 
 //index
 websites.get('/', (req, res) => {
-    res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
 //Create
+websites.post('/', (req, res) => {
+  Websites.create(req.body, (error, createdWebsite) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    }
+    res.status(200).send(createdWebsite);
+  });
+});
 
 //Delete
 
-
 //Update
-
-
 
 module.exports = websites;
